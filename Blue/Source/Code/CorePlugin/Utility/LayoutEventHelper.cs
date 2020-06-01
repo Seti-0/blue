f7fcf79cef6196cf7dc9a63230c85951bbc73821
Kieran <kcosson111@gmail.com>
2020-06-01 16:36:21 +0100

    Allow layouts to respond to Scene graph changes
    Create a helper class which listens for scene graph changes
    and causes affected layouts and layout elements to update.

---
+++ Blue/Source/Code/CorePlugin/Utility/LayoutEventHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Duality;
+using Duality.Resources;
+
+namespace Soulstone.Duality.Plugins.Blue
+{
+    internal static class LayoutEventHelper
+    {
+        public static void Init()
+        {
+            SetupListeners();
+        }
+
+        public static void Shutdown()
+        {
+            ClearListeners();   
+        }
+
+        private static void SetupListeners()
+        {
+            ClearListeners();
+
+            Scene.ComponentAdded += Scene_ComponentAdded;
+            Scene.ComponentRemoving += Scene_ComponentRemoving;
+            Scene.GameObjectParentChanged += Scene_GameObjectParentChanged;
+        }
+
+        private static void ClearListeners()
+        {
+            Scene.ComponentAdded -= Scene_ComponentAdded;
+            Scene.ComponentRemoving -= Scene_ComponentRemoving;
+            Scene.GameObjectParentChanged -= Scene_GameObjectParentChanged;
+        }
+
+        private static void Scene_GameObjectParentChanged(object sender, GameObjectParentChangedEventArgs e)
+        {
+            UpdateLayout(e.OldParent);
+            UpdateLayout(e.Object);
+        }
+
+        private static void Scene_ComponentRemoving(object sender, ComponentEventArgs e)
+        {
+            UpdateLayout(e.Component.GameObj);
+        }
+
+        private static void Scene_ComponentAdded(object sender, ComponentEventArgs e)
+        {
+            UpdateLayout(e.Component.GameObj);
+        }
+
+        private static void UpdateLayout(GameObject obj)
+        {
+            if (obj == null) return;
+
+            var layoutElement = obj.GetComponent<ICmpLayoutElement>();
+            
+            if (layoutElement != null)
+            {
+                layoutElement.UpdateLayoutTree();
+            }
+            else
+            {
+                var layout = obj.GetComponent<ICmpLayout>();
+                layout?.UpdateLayout();
+            }
+        }
+    }
+}
