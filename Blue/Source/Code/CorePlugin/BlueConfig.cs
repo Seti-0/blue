2009d4a6d8680e4131d21c5c394ade0baa59f08b
Kieran <kcosson111@gmail.com>
2020-06-01 16:23:19 +0100

    Add static config class

---
+++ Blue/Source/Code/CorePlugin/BlueConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Duality.Drawing;
+
+namespace Soulstone.Duality.Plugins.Blue
+{
+    public static class BlueConfig
+    {
+        public static VisibilityFlag DefaultUIVisibilityFlag = VisibilityFlag.Group1;
+    }
+}
