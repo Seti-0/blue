9b9a42456168786bfb93d70b53af3e328d06e57a
Seti-0 <kcosson111@gmail.com>
2020-09-18 17:51:37 +0100

    Add metadata related extension methods for properties

---
+++ Blue/Source/Code/CorePlugin/BluePropertyExtMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soulstone.Duality.Plugins.Blue
+{
+    public static class BluePropertyExtMethods
+    {
+        public static bool HasMetadata(this BlueProperty property)
+        {
+            return BluePropertyManager.TryGetMetadata(property, out _);
+        }
+
+        public static BluePropertyMetadata GetMetadata(this BlueProperty property)
+        {
+            if (BluePropertyManager.TryGetMetadata(property, out BluePropertyMetadata data))
+                return data;
+
+            return null;
+        }
+    }
+}
